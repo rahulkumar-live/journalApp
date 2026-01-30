a990fa528ec3c17ba3dcfcf92573c43fe58a3bb4
rahulkumar-live <95327083+rahulkumar-live@users.noreply.github.com>
2026-01-31 02:04:27 +0530

    Redis implementation for cache

--- src/main/java/net/engineeringdigest/journalApp/service/weatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/weatherService.java
@@ -25,11 +25,22 @@ public class weatherService {
     @Autowired
     private AppCache appCache;
 
+    @Autowired
+    private RedisService redisService;
+
     public WeatherResponse getWeather(String city){
-        String finalAPI = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(Placeholders.CITY, city).replace(Placeholders.API_KEY, apiKey);
-        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
-        WeatherResponse body = response.getBody();
-        return body;
+        WeatherResponse weatherResponse = redisService.get("Weather_of_" + city, WeatherResponse.class);
+        if(weatherResponse != null){
+            return weatherResponse;
+        }else{
+            String finalAPI = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(Placeholders.CITY, city).replace(Placeholders.API_KEY, apiKey);
+            ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
+            WeatherResponse body = response.getBody();
+            if(body != null){
+                redisService.set("Weather_of_" + city, body, 300L);
+            }
+            return body;
+        }
     }
 
 }
